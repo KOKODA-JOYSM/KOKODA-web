bd479d4263aab17aba7b24d7c288889c96bc4977
Jeremy Emmanuel <jeremyes25@gmail.com>
2026-04-27 08:37:33 +0700

    build: setup custom colors and typography in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,21 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', 'sans-serif'],
+                quicksand: ['Quicksand', 'sans-serif'],
             },
+            colors: {
+                'base': '#FEFEFE',
+                'background': '#FFF6EC',
+                'highlight': '#FFE7A3',
+                'primary': '#F4C799',
+                'secondary': '#C0976C',
+                'tertiary': '#311A05',
+                'label-found': '#5D8CAD',
+                'label-lost': '#D56666',
+                'gray-text-field': '#777777',
+                'online-color': '#008000',
+            }
         },
     },
 
